a2082affb08333fbcb202b7ea172bdc0c28bb693
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-01-28 20:51:51 +0530

    type error fixed

--- app/(admin)/admin/edit-blog/[blogid]/Client.tsx
+++ app/(admin)/admin/edit-blog/[blogid]/Client.tsx
@@ -6,7 +6,7 @@ import QuillEditor from './QuillEditor';
 
 export type BlogContentType = {
 	title: string;
-	content: string;
+	content: string | undefined;
 	tagsArr: string[];
 	cover_image: string;
 	author: string;
@@ -18,11 +18,11 @@ export default function CreateBlogComponent({ blog }: { blog: Blog }) {
 		title: blog.title,
 		content: blog.content,
 		tagsArr: blog.tagsArr, // Initialize with an empty array
-		cover_image: blog.cover_image,
+		cover_image: blog.cover_image || '',
 		author: blog.author,
 		slug: blog.slug,
 	});
-	const [value, setValue] = useState<string>(blog.content);
+	const [value, setValue] = useState<string | undefined>(blog.content);
 
 	return (
 		<div className='w-full flex flex-col space-y-3 '>
